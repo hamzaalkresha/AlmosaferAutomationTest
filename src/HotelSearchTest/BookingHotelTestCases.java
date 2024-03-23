683011aec55fd8f9d7ab141d6037a94714af00c9
hamzaalkresha <hamzaqwere@gmail.com>
2024-03-23 03:27:24 +0300

    update

--- src/HotelSearchTest/BookingHotelTestCases.java
+++ src/HotelSearchTest/BookingHotelTestCases.java
@@ -1,6 +1,7 @@
 package HotelSearchTest;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -58,5 +59,28 @@ public class BookingHotelTestCases extends Parameters {
 		SearchHotelButton.click();
 
 	}
-
+		@Test(priority = 3)
+		public void PageFullyCompleted() throws InterruptedException {
+			Thread.sleep(12000);
+			WebElement ResultsFound = driver.findElement(By.xpath("//span[@data-testid='HotelSearchResult__resultsFoundCount']"));
+			String ResultsFoundAsText = ResultsFound.getText();
+			
+				
+					Myassert.assertEquals(ResultsFoundAsText.contains("found"), true);
+				}
+		@Test(priority = 4)
+		public void SortPricesFromLowToHigh() throws InterruptedException {
+			WebElement LowestPriceButton = driver.findElement(By.xpath("//button[@data-testid = 'HotelSearchResult__sort__LOWEST_PRICE']"));
+			LowestPriceButton.click();
+			Thread.sleep(2000);
+			WebElement containerOfThePrices = driver.findElement(By.xpath("//div[@class='sc-htpNat KtFsv col-9']"));
+			List<WebElement> prices =  containerOfThePrices.findElements(By.className("Price__Value"));
+			int	FirstPrice = Integer.parseInt(prices.get(0).getText());
+			
+			int LastPrice =Integer.parseInt( prices.get(prices.size()-1).getText());
+			
+			Myassert.assertEquals(FirstPrice<LastPrice , true);														
+			
+		}
 }
+												
